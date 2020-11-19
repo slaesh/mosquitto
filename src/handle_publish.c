016b8ba20454639c5057f71e0fe0de85f0734c64
sascha <sascha@ehle.rs>
2020-11-19 10:14:05 +0100

    add another log msg

--- src/handle_publish.c
+++ src/handle_publish.c
@@ -65,6 +65,8 @@ int handle__publish(struct mosquitto_db *db, struct mosquitto *context)
 		return MOSQ_ERR_PROTOCOL;
 	}
 
+	log__printf(NULL, MOSQ_LOG_NOTICE, "incoming msg");
+
 	payload.ptr = NULL;
 
 	dup = (header & 0x08)>>3;
